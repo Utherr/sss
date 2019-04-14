5abb0d68c50a3f1dd8909551b7ef57a9949d7d7a
Utherr <dd.skora@gmail.com>
2019-04-14 15:00:42 +0200

    checkin is that working

--- src/App.js
+++ src/App.js
@@ -9,16 +9,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Your name here
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            Learning React
+          </h1>
         </header>
       </div>
     );
