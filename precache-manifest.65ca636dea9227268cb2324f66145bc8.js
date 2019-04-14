3d4cabe04330cb80c87f933c57992f789fa3d224
Utherr <dd.skora@gmail.com>
2019-04-14 14:57:28 +0200

    Updates

---
+++ precache-manifest.65ca636dea9227268cb2324f66145bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sss/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "58f4a1ada79ade9c29f8",
+    "url": "/sss/static/js/runtime~main.6ebe8bad.js"
+  },
+  {
+    "revision": "de24a6db84cd3e516012",
+    "url": "/sss/static/js/main.fcba032b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/sss/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "de24a6db84cd3e516012",
+    "url": "/sss/static/css/main.4e2c76e1.chunk.css"
+  },
+  {
+    "revision": "24442cdad0041606436accc08e17dd4c",
+    "url": "/sss/index.html"
+  }
+];
